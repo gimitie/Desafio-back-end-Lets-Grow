cafb6cf8851a4d1e5fb3f21ab8afa19270bf439f
Giovana Mitie <93398358+gimitie@users.noreply.github.com>
2021-11-22 21:50:36 -0300

    Add files via upload

---
+++ ecommerce/src/main/java/br/com/ecommerce/controller/ContatoController.java
@@ -0,0 +1,15 @@
+package br.com.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContatoController {
+
+	@GetMapping("/contato")
+	public String contato(Model model) {
+		
+		return "contato";
+	}
+}
